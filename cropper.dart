7db7dba1c6b710e70ee4fe634c5708649decfa92
Ali <38964898+seyedalidev2002@users.noreply.github.com>
2024-03-28 17:26:30 +0330

    Update cropper.dart

--- cropper.dart
+++ cropper.dart
@@ -4,8 +4,6 @@ import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/custom_code/widgets/index.dart'; // Imports other custom widgets
-import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
